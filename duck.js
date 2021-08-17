067305934458a01eab518c062984146495631140
kewlamogh <amogh_sarma@outlook.com>
2021-08-17 08:56:55 -0700

    initial

--- duck.js
+++ duck.js
@@ -1,1 +1,26 @@
+class Duck {
+    constructor (config) {
+        this.parent = (config.appElement == "body" ? document.body : document.getElementById(config.appElement));
+        document.title = config.title;
+        for (var i in config.styles) {
+            this.parent.style[i] = config.styles[i];
+        }
+        this.config = config
+        this.vars = config.vars
+    }
+    newElement(elem, attrs, text, styles = this.config) {
+        let editedComponent = text;
+        for (var i in this.vars) {
+            editedComponent = editedComponent.replace("{{"+i+"}}", this.vars[i])
+        }
 
+        let newElement = document.createElement(elem);
+        for (var i in attrs) {
+            newElement[i] = attrs[i];
+        }
+        newElement.innerText = editedComponent;
+
+        console.log(newElement.href)
+        this.parent.appendChild(newElement);
+    }
+}
---
+++ test.js
@@ -0,0 +1,19 @@
+let duck = new Duck({
+    appElement: "body",
+    title: "hello world!",
+    styles: {
+        "color": "red",
+        "background-color": "black"
+    },
+    vars: {
+        "maintext": "Hello World!!",
+        "random number": Math.round(Math.random() * 600)
+    }
+})
+
+duck.newElement("a", {
+    "href": "https://replit.com"
+}, "{{maintext}} {{random number}} is a random number. Click here!", {
+    "text-decoration": "none",
+    "color": "blue"
+})
